f0cc52de4d5cd7c3f5fe1df99be93be70053aaaa
Martin Pugh <pugh@s3kr.it>
2020-03-11 13:45:47 +0100

    clone the repo

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@
 FROM alpine:latest
 
 RUN apk add --update --no-cache git git-lfs
+RUN mkdir /repo
+WORKDIR /repo
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-set -eu
-
-printenv
+set -eux
 
 git_setup ( ) {
-  cat <<- EOF > $HOME/.netrc
+  cat <<- EOF > "$HOME/.netrc"
         machine github.com
         login $GITHUB_ACTOR
         password $GITHUB_TOKEN
@@ -12,17 +10,18 @@ git_setup ( ) {
         login $GITHUB_ACTOR
         password $GITHUB_TOKEN
 EOF
-  chmod 600 $HOME/.netrc
+  chmod 600 "$HOME/.netrc"
 
   git config --global user.email "actions@github.com"
   git config --global user.name "Github Actions"
+  git clone "https://github.com/${GITHUB_REPOSITORY}" .
 }
 
 echo "Setting up git machine..."
 git_setup
 
 echo "Forcing tag update..."
-git tag -a ${INPUT_TAG_NAME} -m "${INPUT_TAG_MESSAGE}" "${GITHUB_SHA}" -f
+git tag -a "${INPUT_TAG_NAME}" -m "${INPUT_TAG_MESSAGE}" "${GITHUB_SHA}" -f
 
 echo "Forcing tag push..."
-git push -f origin refs/tags/${INPUT_TAG_NAME}
+git push -f origin "refs/tags/${INPUT_TAG_NAME}"
